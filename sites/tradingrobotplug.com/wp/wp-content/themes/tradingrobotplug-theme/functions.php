7f965bf33723f4d66a47f3f4105599cfff4ae2d1
OrganizerApp <organizer@app.local>
2026-01-07 17:51:34 -0600

    agent-8: ollama integration + directory audit completion

--- sites/tradingrobotplug.com/wp/wp-content/themes/tradingrobotplug-theme/functions.php
+++ sites/tradingrobotplug.com/wp/wp-content/themes/tradingrobotplug-theme/functions.php
@@ -264,13 +264,14 @@ add_filter('template_include', function ($template) {
  */
 function add_analytics_tracking_codes() {
     // Google Analytics 4 (GA4)
+    if (defined('GA4_MEASUREMENT_ID') && GA4_MEASUREMENT_ID) {
         echo '<!-- Google Analytics 4 (GA4) -->\n';
-        echo '<script async src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXXXXX"></script>\n';
+        echo '<script async src="https://www.googletagmanager.com/gtag/js?id=' . GA4_MEASUREMENT_ID . '"></script>\n';
         echo '<script>\n';
         echo 'window.dataLayer = window.dataLayer || [];\n';
         echo 'function gtag(){dataLayer.push(arguments);}\n';
         echo 'gtag(\'js\', new Date());\n';
-        echo 'gtag(\'config\', \'G-XXXXXXXXXX\', {\n';
+        echo 'gtag(\'config\', \'' . GA4_MEASUREMENT_ID . '\', {\n';
         echo '\'page_path\': window.location.pathname,\n';
         echo '\'page_title\': document.title,\n';
         echo '});\n';
@@ -287,8 +288,10 @@ function add_analytics_tracking_codes() {
         echo '});\n';
         echo '</script>\n';
         echo '<!-- End GA4 -->\n';
-    
+    }
+
     // Facebook Pixel
+    if (defined('FACEBOOK_PIXEL_ID') && FACEBOOK_PIXEL_ID) {
         echo '<!-- Facebook Pixel Code -->\n';
         echo '<script>\n';
         echo '!function(f,b,e,v,n,t,s)\n';
@@ -299,14 +302,15 @@ function add_analytics_tracking_codes() {
         echo 't.src=v;s=b.getElementsByTagName(e)[0];\n';
         echo 's.parentNode.insertBefore(t,s)}}(window, document,\'script\',\n';
         echo '\'https://connect.facebook.net/en_US/fbevents.js\');\n';
-        echo 'fbq(\'init\', \'YOUR_PIXEL_ID\');\n';
+        echo 'fbq(\'init\', \'' . FACEBOOK_PIXEL_ID . '\');\n';
         echo 'fbq(\'track\', \'PageView\');\n';
         echo '</script>\n';
         echo '<noscript>\n';
         echo '<img height="1" width="1" style="display:none"\n';
-        echo 'src="https://www.facebook.com/tr?id=YOUR_PIXEL_ID&ev=PageView&noscript=1"/>\n';
+        echo 'src="https://www.facebook.com/tr?id=' . FACEBOOK_PIXEL_ID . '&ev=PageView&noscript=1"/>\n';
         echo '</noscript>\n';
         echo '<!-- End Facebook Pixel Code -->\n';
+    }
 }
 add_action('wp_head', 'add_analytics_tracking_codes', 99);
 
